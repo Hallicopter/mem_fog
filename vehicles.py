9eaa86ab38051b7ba1725002d713cee22cf67ad8
Hallicopter <advait.raykar@gmail.com>
2019-04-10 14:18:57 +0530

    makes vehicles.py send multiple vehicles sdata at once

--- vehicles.py
+++ vehicles.py
@@ -23,28 +23,25 @@ class Vehicle:
 			each second for 100 seconds
 		"""
 		global parents
-		for i in range(100):
-			time.sleep(0.1)
-			speed 			= random.randint(1, 101) 				# select a random speed
-			parent_index 	= random.randint(0, len(parents)-1) 	# select a parent out of the available with eq probability
-			payload			= {'id':self.id, 'speed':speed}
-			ret 			= requests.get(parents[parent_index] + 'recv_data/', 
-								params={'json':json.dumps(payload)})
-			
-			# url = parents[parent_index] #+ 'recieve_data'
-			# print(url)
-			# ret = requests.get(url)
-
-			if ret:
-				print("Vehicle {} successfully sent data to {}".format(self.id, parents[parent_index]))
-			else:
-				print("Vehicle {} counldn't send data to {}".format(self.id, parents[parent_index]))
+		
+		time.sleep(0.01)
+		speed 			= random.randint(1, 101) 				# select a random speed
+		parent_index 	= random.randint(0, len(parents)-1) 	# select a parent out of the available with eq probability
+		payload			= {'id':self.id, 'speed':speed}
+		ret 			= requests.get(parents[parent_index] + 'recv_data/', 
+							params={'json':json.dumps(payload)})
+		
+		if ret:
+			print("Vehicle {} successfully sent data to {}".format(self.id, parents[parent_index]))
+		else:
+			print("Vehicle {} counldn't send data to {}".format(self.id, parents[parent_index]))
 
 
 @app.route('/simulate_vehicles', methods=['GET','POST'])
 def send_data():
-	for i in range(int(request.data)):
-		Vehicle(i)
+	for i in range(100):
+		for i in range(int(request.data)):
+			Vehicle(i)
 	return ACK
 
 @app.route('/service_coordination/notify_divide')
